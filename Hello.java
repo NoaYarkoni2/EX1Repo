063cfe812caf68e8e9d37cb49db23410c820a55f
NoaYarkoni2 <97391509+NoaYarkoni2@users.noreply.github.com>
2023-03-19 19:15:04 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 		public static void main (String[] arga){
 		for (int i=1; i<=10; i++){
-				System.out.println("Hello World ..."+i);
+				System.out.println("Hello Raz and Noa..."+i);
 				}
 			}
 		}
